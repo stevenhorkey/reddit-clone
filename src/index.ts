da19b038220e86a8e1b2b51635b6bb8a4367fa44
Steven Horkey <steven@Stevens-MacBook-Pro.local>
2020-11-22 11:49:49 -0700

    crud via graphql

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
+import "reflect-metadata";
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
-import { Post } from "./entities/Post";
+// import { Post } from "./entities/Post";
 import mikroConfig from "./mikro-orm.config";
+import express from 'express';
+import {ApolloServer} from 'apollo-server-express';
+import {buildSchema} from 'type-graphql';
+import { HelloResolver } from "./resolvers/hello";
+import { PostResolver } from "./resolvers/post";
 
 const main = async () => {
     // connect to database
@@ -16,6 +22,25 @@ const main = async () => {
     // get all posts
     // const posts = await orm.em.find(Post, {});
     // console.log(posts);
+
+    const app = express();
+
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers: [HelloResolver, PostResolver],
+            validate: false
+        }),
+        context: () => ({ em: orm.em })
+    });
+
+    apolloServer.applyMiddleware({ app })
+
+    app.get('/', (_, res) => {
+        res.send('hello world');
+    });
+    app.listen(4000, () => {
+        console.log('server started on localhost:4000');
+    });
 }
 
 main();
